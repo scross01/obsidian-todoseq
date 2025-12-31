0ca8c7bc795f5898d2abfcc1aa1f2fc1b71e0edc
Stephen Cross <scross01@gmail.com>
2025-12-31 08:22:47 -0500

    remove redundant unused code

--- tests/test-setup.ts
+++ tests/test-setup.ts
@@ -1,14 +1,12 @@
 // Test setup file for Jest
 import { LanguageRegistry } from '../src/parser/language-registry';
-import { LanguageAwareRegexBuilder } from '../src/parser/code-block-tasks';
 import { DEFAULT_PENDING_STATES, DEFAULT_ACTIVE_STATES, DEFAULT_COMPLETED_STATES } from '../src/task';
 
 // Create a global registry instance for all tests
 const registry = new LanguageRegistry();
-const regexBuilder = new LanguageAwareRegexBuilder();
 
 // Export test utilities
-export { registry, regexBuilder, DEFAULT_PENDING_STATES, DEFAULT_ACTIVE_STATES, DEFAULT_COMPLETED_STATES };
+export { registry, DEFAULT_PENDING_STATES, DEFAULT_ACTIVE_STATES, DEFAULT_COMPLETED_STATES };
 
 // Mock console methods to reduce noise during tests
 global.console = {
